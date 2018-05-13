43fb33ceeabbf6aba22c0a4ee39ab05f6f344730
Michael k <michaelkahnt123@gmail.com>
2018-05-13 17:17:42 +0200

    Add files via upload

---
+++ alle tastenkuerzel.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+cout << "           |Alle Tastenkuerzel|" << endl
+	 << "strg + s =                           speichern" << endl
+	 << "strg + o =                       datei oeffnen" << endl
+	 << "strg + c =                            kopieren" << endl
+	 << "strg + v =                           einfuegen" << endl
+	 << "strg + f =                       zeile loeshen" << endl
+	 << "strg + e =   zeile in naechste spalte kopieren" << endl
+	 << "strg + n =                     neue quelldatei" << endl
+	 << "strg + z =                        rueckgaengig" << endl
+	 << "strg + y =                   wiederherstellen " << endl
+	 << "strg + m = zwei quelltexte nebeneinander legen" << endl
+	 << "strg + . =          kommentar slashes erzeugen" << endl
+	 << "strg + a =                      alles makieren" << endl
+	 << "Shift + pfeil beliebige richtung =    makieren" << endl;
+
+return 0;
+	
+}
