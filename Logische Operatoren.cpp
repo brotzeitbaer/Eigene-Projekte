0b8244045a6c42af1480a4aea6d9aeb9789744b4
Michael k <michaelkahnt123@gmail.com>
2020-01-20 16:33:55 +0100

    Create Logische Operatoren.cpp

---
+++ Logische Operatoren.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	/* Logische Operatoren: 
+
+	   if ((variable1 == 't') || (variable1 == 'T'))
+	   if ((variable2 == 'b') && (variable2 == 'B'))
+	   
+	   Die zwei || stehehn hierbei für oder.
+	   Die zwei && stehen hierbei für und.
+	   
+	   */
+
+	return 0;
+}
