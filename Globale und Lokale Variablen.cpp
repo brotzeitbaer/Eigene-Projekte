291e3707ab6930184b55a477c905314fc56a367d
Michael k <michaelkahnt123@gmail.com>
2020-01-27 16:52:13 +0100

    Create Globale und Lokale Variablen.cpp

---
+++ Globale und Lokale Variablen.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+/*Das ist eine Globale variable. Sie kann in jedem Codeblock verwendet werden.*/
+int bonus = 500;
+
+int main()
+{
+	/*Das ist eine Lokale variable. Sie kann nur innerhalb dieses Codeblocks verwendet werden und
+	innerhalb Codeblöcke dieses Codeblocks.*/
+	int punkte;
+
+	return 0;
+
+}
